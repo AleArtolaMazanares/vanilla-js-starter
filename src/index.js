145fa2f90d640ed3c368fc151e00033ac4054a1a
Alejandro Artola <aartolam@fwdcostarica.com>
2023-07-03 15:58:52 -0600

    terminado modulado

--- src/index.js
+++ src/index.js
@@ -1,2 +1,51 @@
-// Inserte el código aquí
+////////////////////////////////// IMPORTACIONES //////////////////////////////
+import {
+  validar,
+  ValidarDatos,
+  esMayuscula,
+  btn,
+  input,
+  ul,
+  parrafo,
+  checkBox,
+  BtnEliminar,
+} from "./prueba.js";
 
+////////////////////////////////// IMPORTACIONES //////////////////////////////
+
+////////////////////////////////// VALIDAR CAMPO //////////////////////////////
+btn.addEventListener("click", validar, input);
+////////////////////////////////// VALIDAR CAMPO //////////////////////////////
+
+//////////////////////////////////  UNIR TODO //////////////////////////////
+
+btn.addEventListener("click", (e) => {
+  e.preventDefault;
+  let texto = input.value;
+  if (esMayuscula(texto)) {
+    if (ValidarDatos(texto)) {
+      if (texto !== "") {
+        let li = document.createElement("li");
+        li.className = "li";
+        let p = document.createElement("p");
+
+        p.textContent = texto;
+
+        input.value = "";
+
+        ul.appendChild(li);
+        li.appendChild(checkBox());
+        li.appendChild(p);
+        li.appendChild(BtnEliminar());
+
+        parrafo.style.display = "none";
+      }
+    } else {
+      alert("La tarea que ingresaste esta repetida");
+    }
+    input.value = "";
+  } else {
+    alert("La tarea que ingresaste esta repetida");
+  }
+});
+//////////////////////////////////  UNIR TODO //////////////////////////////
---
+++ src/prueba.js
@@ -0,0 +1,117 @@
+////////////////////////////// VARIABLES //////////////////////////////////
+let input = document.querySelector("#input");
+let btn = document.querySelector("#btn-agregar");
+let ul = document.querySelector("ul");
+let parrafo = document.querySelector(".parrafo");
+let contador = document.querySelector(".contador");
+
+////////////////////////////// EXPORTACIONES //////////////////////////////////
+
+export {
+  validar,
+  ValidarDatos,
+  esMayuscula,
+  btn,
+  input,
+  ul,
+  parrafo,
+  checkBox,
+  BtnEliminar,
+};
+////////////////////////////// EXPORTACIONES //////////////////////////////////
+
+/////////////////////////////////// FUNCIONES /////////////////////////////////
+
+////////////////////////// FUNCION CAMPOS VACIOS /////////////////////////////////
+function validar() {
+  let datos = input.value;
+  if (datos.length == 0) {
+    alert("campo invalido");
+  }
+}
+
+////////////////////////// FUNCION CAMPOS VACIOS /////////////////////////////////
+
+///////////////////////// FUNCIONE PARA VALIDAR DATOS /////////////////////////////////
+function ValidarDatos(datos) {
+  let li = ul.getElementsByTagName("li");
+
+  let caracter = datos.charAt(0);
+  console.log(caracter);
+  for (let e = 0; e < li.length; e++) {
+    if (li[e].innerText === datos) {
+      return false;
+    }
+  }
+  return true;
+}
+
+///////////////////////// FUNCIONE PARA VALIDAR DATOS /////////////////////////////////
+
+///////////////////////// FUNCIONE INDENTIFICAR SI ES MAYUSCULA /////////////////////////////////
+function esMayuscula() {
+  let datos = input.value;
+  let elementos = ul.getElementsByTagName("li");
+
+  for (let e = 0; e < elementos.length; e++) {
+    if (elementos[e].textContent.toUpperCase() == datos.toUpperCase()) {
+      return false;
+    }
+  }
+  return true;
+}
+
+///////////////////////// FUNCIONE INDENTIFICAR SI ES MAYUSCULA /////////////////////////////////
+
+////////////////////////////////// FUNCION BTN ELIMINAR /////////////////////////////
+
+function BtnEliminar() {
+  let Eliminar = document.createElement("i");
+
+  Eliminar.className = "fa fa-trash";
+
+  Eliminar.addEventListener("click", (e) => {
+    let item = e.target.parentElement;
+    let checkBox = item.querySelector("input");
+    if (checkBox.checked) {
+      let cuenta = Number(contador.textContent);
+      cuenta = cuenta - 1;
+      contador.textContent = cuenta;
+    }
+    ul.removeChild(item);
+
+    let items = ul.getElementsByTagName("li");
+    if (items.length == 0) {
+      parrafo.style.display = "block";
+    }
+  });
+
+  return Eliminar;
+}
+////////////////////////////////// FUNCION BTN ELIMINAR /////////////////////////////
+
+////////////////////////////////// FUNCION CHECKBOX /////////////////////////////
+function checkBox() {
+  let btnCheck = document.createElement("input");
+  btnCheck.setAttribute("type", "checkbox");
+
+  btnCheck.className = "btnCheckBox";
+
+  btnCheck.addEventListener("click", function () {
+    if (btnCheck.checked) {
+      let cuenta = Number(contador.textContent);
+      cuenta = cuenta + 1;
+      contador.textContent = cuenta;
+    } else if (contador.textContent > 0) {
+      let cuenta = Number(contador.textContent);
+      cuenta = cuenta - 1;
+      contador.textContent = cuenta;
+    } else if (btnCheck.checked) {
+    }
+  });
+
+  return btnCheck;
+}
+////////////////////////////////// FUNCION CHECKBOX /////////////////////////////
+
+/////////////////////////////////// FUNCIONES /////////////////////////////////
